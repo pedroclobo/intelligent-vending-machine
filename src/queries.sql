ff26c394997dd870c794b7a50c4543c2c540e6bc
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-16 10:46:03 +0100

    Finish queries

---
+++ src/queries.sql
@@ -0,0 +1,28 @@
+SELECT name
+FROM responsavel_por NATURAL JOIN retalhista
+GROUP BY name
+HAVING COUNT(DISTINCT nome_cat) >= ALL (
+	SELECT COUNT(DISTINCT nome_cat)
+	FROM responsavel_por
+	GROUP BY name
+);
+
+
+SELECT name
+FROM responsavel_por NATURAL JOIN retalhista
+GROUP BY name
+HAVING COUNT(DISTINCT nome_cat) = (
+	SELECT COUNT(*)
+	FROM categoria_simples
+);
+
+
+(SELECT ean FROM produto)
+EXCEPT
+(SELECT ean FROM evento_reposicao);
+
+
+SELECT ean
+FROM evento_reposicao
+GROUP BY ean
+HAVING COUNT(DISTINCT tin) = 1;
