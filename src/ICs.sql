397349584abad02527e79e54d29100f0cf28c1f4
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-17 20:39:51 +0100

    Implement RI-1

--- src/ICs.sql
+++ src/ICs.sql
@@ -1,5 +1,42 @@
+DROP TRIGGER IF EXISTS ri_1 ON tem_outra;
 DROP TRIGGER IF EXISTS ri_4 ON evento_reposicao;
 DROP TRIGGER IF EXISTS ri_5 ON evento_reposicao;
+
+CREATE OR REPLACE FUNCTION ri_1_proc()
+RETURNS TRIGGER
+AS $$
+DECLARE
+	cat_original VARCHAR(255);
+	cat VARCHAR(255);
+	super_cat VARCHAR(255);
+	contador INT;
+BEGIN
+	cat_original = new.nome_categoria;
+	cat = new.nome_categoria;
+	super_cat = new.nome_super_categoria;
+	contador = 1;
+
+	WHILE contador <> 0 LOOP
+		IF (SELECT nome_super_categoria FROM tem_outra WHERE nome_categoria = super_cat) = cat_original THEN
+			RAISE EXCEPTION	'Uma categoria não pode estar contida em si própria.';
+		END IF;
+
+		cat = super_cat;
+
+		SELECT nome_super_categoria INTO super_cat
+		FROM tem_outra
+		WHERE nome_categoria = cat;
+
+		SELECT COUNT(distinct nome_super_categoria) INTO contador
+		FROM tem_outra
+		WHERE nome_categoria = cat;
+
+	END LOOP;
+
+	RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION ri_4_proc()
 RETURNS TRIGGER
@@ -43,6 +80,13 @@ END
 $$
 LANGUAGE plpgsql;
 
+CREATE TRIGGER ri_1_insert
+BEFORE INSERT ON tem_outra
+FOR EACH ROW EXECUTE PROCEDURE ri_1_proc();
+
+CREATE TRIGGER ri_1_update
+BEFORE UPDATE ON tem_outra
+FOR EACH ROW EXECUTE PROCEDURE ri_1_proc();
 
 CREATE TRIGGER ri_4_insert
 BEFORE INSERT ON evento_reposicao
